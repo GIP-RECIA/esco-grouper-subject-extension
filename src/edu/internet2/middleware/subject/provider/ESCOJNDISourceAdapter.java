5fee32bb92e47660b07e2f6605fdf338117f39ce
Julien Gribonvald <julien.gribonvald@gmail.com>
2010-11-16 14:01:10 +0000

    mise à jour pour grouper 1.5.2

--- src/edu/internet2/middleware/subject/provider/ESCOJNDISourceAdapter.java
+++ src/edu/internet2/middleware/subject/provider/ESCOJNDISourceAdapter.java
@@ -1,6 +1,5 @@
 package edu.internet2.middleware.subject.provider;
 
-
 import edu.internet2.middleware.subject.Subject;
 import edu.internet2.middleware.subject.SubjectNotFoundException;
 import edu.internet2.middleware.subject.SubjectNotUniqueException;
@@ -17,12 +16,11 @@ import javax.naming.directory.SearchResult;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-
 /**
- * ESCOJNDISourceAdapter that add the notion of a context defined by an attribute value.
- * @author GIP RECIA - A. Deman
- * 1 oct. 2009
- *
+ * ESCOJNDISourceAdapter that add the notion of a context defined by an
+ * attribute value.
+ * 
+ * @author GIP RECIA - A. Deman 1 oct. 2009
  */
 public class ESCOJNDISourceAdapter extends JNDISourceAdapter {
 
@@ -41,44 +39,46 @@ public class ESCOJNDISourceAdapter extends JNDISourceAdapter {
     public ESCOJNDISourceAdapter() {
         super();
     }
-   
+
     /**
      * {@inheritDoc}
      */
     @Override
-    public Subject getSubject(final String subjectId)
-    throws SubjectNotFoundException, SubjectNotUniqueException {
+    public Subject getSubject(final String subjectId, final boolean exceptionIfNull)
+            throws SubjectNotFoundException, SubjectNotUniqueException {
         Subject subject = null;
-        Search search = getSearch("searchSubject");
+        Search search = this.getSearch("searchSubject");
         if (search == null) {
             LOGGER.error("searchType: \"searchSubject\" not defined.");
             return subject;
         }
-        String[] attributeNames = {nameAttributeName, descriptionAttributeName, subjectIDAttributeName};
-        Attributes subjAttributes = getLdapUnique(search, subjectId, attributeNames);
-        subject = createSubject(subjAttributes);
+        String[] attributeNames = {this.nameAttributeName, this.descriptionAttributeName,
+                this.subjectIDAttributeName };
+        Attributes subjAttributes = this.getLdapUnique(search, subjectId, attributeNames);
+        subject = this.createSubject(subjAttributes);
         if (subject == null) {
             throw new SubjectNotFoundException("Subject " + subjectId + " not found.");
         }
-        
+
         return subject;
     }
-    
+
     /**
      * {@inheritDoc}
      */
     @Override
-    public Subject getSubjectByIdentifier(final String subjectId)
-    throws SubjectNotFoundException, SubjectNotUniqueException {
+    public Subject getSubjectByIdentifier(final String subjectId, final boolean exceptionIfNull)
+            throws SubjectNotFoundException, SubjectNotUniqueException {
         Subject subject = null;
-        Search search = getSearch("searchSubjectByIdentifier");
+        Search search = this.getSearch("searchSubjectByIdentifier");
         if (search == null) {
             LOGGER.error("searchType: \"searchSubjectByIdentifier\" not defined.");
             return subject;
         }
-        String[] attributeNames = {nameAttributeName, subjectIDAttributeName, descriptionAttributeName};
-        Attributes subjectAttributes = getLdapUnique(search, subjectId, attributeNames);
-        subject = createSubject(subjectAttributes);
+        String[] attributeNames = {this.nameAttributeName, this.subjectIDAttributeName,
+                this.descriptionAttributeName };
+        Attributes subjectAttributes = this.getLdapUnique(search, subjectId, attributeNames);
+        subject = this.createSubject(subjectAttributes);
         if (subject == null) {
             throw new SubjectNotFoundException("Subject " + subjectId + " not found.");
         }
@@ -89,22 +89,66 @@ public class ESCOJNDISourceAdapter extends JNDISourceAdapter {
      * {@inheritDoc}
      */
     @Override
-    public Set<Subject> search(final String searchString) {
+    public Subject getSubject(final String subjectId) throws SubjectNotFoundException, SubjectNotUniqueException {
+        Subject subject = null;
+        Search search = this.getSearch("searchSubject");
+        if (search == null) {
+            LOGGER.error("searchType: \"searchSubject\" not defined.");
+            return subject;
+        }
+        String[] attributeNames = {this.nameAttributeName, this.descriptionAttributeName,
+                this.subjectIDAttributeName };
+        Attributes subjAttributes = this.getLdapUnique(search, subjectId, attributeNames);
+        subject = this.createSubject(subjAttributes);
+        if (subject == null) {
+            throw new SubjectNotFoundException("Subject " + subjectId + " not found.");
+        }
 
-        final Set<Subject> result = new HashSet<Subject>();
-        Search search = getSearch("search");
+        return subject;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Subject getSubjectByIdentifier(final String subjectId) throws SubjectNotFoundException,
+            SubjectNotUniqueException {
+        Subject subject = null;
+        Search search = this.getSearch("searchSubjectByIdentifier");
+        if (search == null) {
+            LOGGER.error("searchType: \"searchSubjectByIdentifier\" not defined.");
+            return subject;
+        }
+        String[] attributeNames = {this.nameAttributeName, this.subjectIDAttributeName,
+                this.descriptionAttributeName };
+        Attributes subjectAttributes = this.getLdapUnique(search, subjectId, attributeNames);
+        subject = this.createSubject(subjectAttributes);
+        if (subject == null) {
+            throw new SubjectNotFoundException("Subject " + subjectId + " not found.");
+        }
+        return subject;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Set < Subject > search(final String searchString) {
+
+        final Set < Subject > result = new HashSet < Subject >();
+        Search search = this.getSearch("search");
         String searchExpression;
 
-        // If an scope value is found in the search string 
-        // the string is decomposed and a decorated Search instance is used. 
-        final int index = searchString .indexOf(SCOPE_DELIM);
+        // If an scope value is found in the search string
+        // the string is decomposed and a decorated Search instance is used.
+        final int index = searchString.indexOf(ESCOJNDISourceAdapter.SCOPE_DELIM);
         if (index >= 0) {
             final String searchTerm = searchString.substring(0, index).trim();
-            final String scopeTerm = searchString.substring(index + SCOPE_DELIM.length()).trim();
-            final String[] scopes = scopeTerm.split(SCOPE_SEP);
+            final String scopeTerm = searchString.substring(index + ESCOJNDISourceAdapter.SCOPE_DELIM.length()).trim();
+            final String[] scopes = scopeTerm.split(ESCOJNDISourceAdapter.SCOPE_SEP);
             search = new ESCOSearchWithScopeDecorator(scopes, search);
             searchExpression = searchTerm;
-        }  else {
+        } else {
             searchExpression = searchString;
         }
 
@@ -116,7 +160,7 @@ public class ESCOJNDISourceAdapter extends JNDISourceAdapter {
                 this.descriptionAttributeName, };
 
         @SuppressWarnings("unchecked")
-        NamingEnumeration ldapResults = getLdapResults(search, searchExpression, attributeNames);
+        NamingEnumeration ldapResults = this.getLdapResults(search, searchExpression, attributeNames);
         if (ldapResults == null) {
             return result;
         }
@@ -124,7 +168,7 @@ public class ESCOJNDISourceAdapter extends JNDISourceAdapter {
             while (ldapResults.hasMore()) {
                 SearchResult si = (SearchResult) ldapResults.next();
                 Attributes attributes1 = si.getAttributes();
-                Subject subject = createSubject(attributes1);
+                Subject subject = this.createSubject(attributes1);
                 result.add(subject);
             }
         } catch (NamingException ex) {
@@ -137,46 +181,45 @@ public class ESCOJNDISourceAdapter extends JNDISourceAdapter {
 
     /**
      * Creates a subject with a custom implementation.
-     * @param attrs  The attributes associated to the subject.
-     * @return  The Subject.
+     * 
+     * @param attrs
+     *            The attributes associated to the subject.
+     * @return The Subject.
      */
     private Subject createSubject(final Attributes attrs) {
         String subjectName = "";
         String subjectID = "";
         String description = "";
         try {
-            Attribute attribute = attrs.get(subjectIDAttributeName);
+            Attribute attribute = attrs.get(this.subjectIDAttributeName);
             if (attribute == null) {
-                LOGGER.error("The LDAP attribute \"" 
-                            + subjectIDAttributeName 
-                            + "\" does not have a value. "
-                            + "It is beging used as the Grouper special attribute \"SubjectID\".");
+                LOGGER.error("The LDAP attribute \"" + this.subjectIDAttributeName
+                        + "\" does not have a value. "
+                        + "It is beging used as the Grouper special attribute \"SubjectID\".");
                 return null;
             }
-            subjectID   = (String) attribute.get();
-            attribute = attrs.get(nameAttributeName);
+            subjectID = (String) attribute.get();
+            attribute = attrs.get(this.nameAttributeName);
             if (attribute == null) {
-                LOGGER.error("The LDAP attribute \"" 
-                        + nameAttributeName 
-                        + "\" does not have a value. z" 
+                LOGGER.error("The LDAP attribute \"" + this.nameAttributeName
+                        + "\" does not have a value. z"
                         + "It is being used as the Grouper special attribute \"name\".");
                 return null;
             }
             subjectName = (String) attribute.get();
-            attribute = attrs.get(descriptionAttributeName);
+            attribute = attrs.get(this.descriptionAttributeName);
             if (attribute == null) {
-                LOGGER.error("The LDAP attribute \"" 
-                        + descriptionAttributeName 
+                LOGGER.error("The LDAP attribute \"" + this.descriptionAttributeName
                         + "\" does not have a value. "
                         + "It is being used as the Grouper special attribute \"description\".");
             } else {
-                description   = (String) attribute.get();
+                description = (String) attribute.get();
             }
         } catch (NamingException ex) {
             LOGGER.error("LDAP Naming Except: " + ex.getMessage(), ex);
         }
-        
-        return new ESCOJNDISubject(subjectID,  subjectName, description, this.getSubjectType(), this);
-           
+
+        return new ESCOJNDISubject(subjectID, subjectName, description, this.getSubjectType(), this);
+
     }
 }
--- src/edu/internet2/middleware/subject/provider/ESCOJNDISubject.java
+++ src/edu/internet2/middleware/subject/provider/ESCOJNDISubject.java
@@ -1,24 +1,26 @@
 /**
- * 
+ *
  */
 package edu.internet2.middleware.subject.provider;
 
 import edu.internet2.middleware.subject.SubjectType;
-
 import java.util.Set;
 
-
 /**
- * Adds the support for the multi-valued attributes to the
- * getAttributeValue method.
- * @author GIP RECIA - A. Deman
- * 2 oct. 2009
- *
+ * Adds the support for the multi-valued attributes to the getAttributeValue
+ * method.
+ * 
+ * @author GIP RECIA - A. Deman 2 oct. 2009
  */
 public class ESCOJNDISubject extends JNDISubject {
-    
+
+    /**
+     * The default generated uid.
+     */
+    private static final long   serialVersionUID = 6687997365345098962L;
+
     /** The separator for the values of a multi-valued attribute. */
-    private static final String SEP = ";";
+    private static final String SEP              = ";";
 
     /**
      * Builds an instance of ESCOJNDISubject.
@@ -28,21 +30,19 @@ public class ESCOJNDISubject extends JNDISubject {
      * @param type The type of subject.
      * @param adapter The source adapter used to retrieve the subject.
      */
-    public ESCOJNDISubject(final String id, final String name, final String description,
-            final SubjectType type, final JNDISourceAdapter adapter) {
-        super(id, name, description, type, adapter);
+    public ESCOJNDISubject(final String id, final String name, final String description, final SubjectType type,
+            final JNDISourceAdapter adapter) {
+        super(id, name, description, type.getName(), adapter.getId());
     }
-    
-    
-    
+
     /**
      * {@inheritDoc}
      */
     @Override
     public String getAttributeValue(final String attName) {
-       
+
         @SuppressWarnings("unchecked")
-        Set<String> values = getAttributeValues(attName);
+        Set < String > values = this.getAttributeValues(attName);
         if (values == null) {
             return null;
         }
@@ -50,14 +50,13 @@ public class ESCOJNDISubject extends JNDISubject {
         boolean separate = false;
         for (String value : values) {
             if (separate) {
-                result +=  SEP;
+                result += ESCOJNDISubject.SEP;
             } else {
                 separate = true;
             }
             result += value;
         }
-       return result;
+        return result;
     }
-
 
 }
